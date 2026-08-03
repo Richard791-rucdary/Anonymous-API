d19684a598a72ccd46d573792e34bc606309fb25
Richard791-rucdary <badgcheets@gmail.com>
2026-08-03 04:27:00 +0100

    Update MongoDB driver imports and ObjectID usage

--- main.go
+++ main.go
@@ -11,11 +11,12 @@ import (
 
 "encoding/json"
 
-"go.mongodb.org/mongo-driver/v2/bson"
+"go.mongodb.org/mongo-driver/bson"
 
-"go.mongodb.org/mongo-driver/v2/mongo"
+"go.mongodb.org/mongo-driver/mongo"
 
-"go.mongodb.org/mongo-driver/v2/mongo/options"
+"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 
@@ -23,7 +24,7 @@ import (
 var collect *mongo.Collection
 type User struct {
 
-		ID bson.ObjectID `bson:"_id,omitempty" json:"ID"`
+		ID primitive.ObjectID `bson:"_id,omitempty" json:"ID"`
 
 		Text string `bson:"text" json:"text"`
 	}
@@ -128,7 +129,7 @@ write.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 				write.WriteHeader(http.StatusBadRequest)
 				return
 		}
-		obj, err := bson.ObjectIDFromHex(id)
+		obj, err := primitive.ObjectIDFromHex(id)
 		if err != nil {
 				http.Error(write, "Invalid ID, check your ID", http.StatusBadRequest)
 				return
